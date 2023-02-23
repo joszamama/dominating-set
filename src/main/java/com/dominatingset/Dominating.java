1b8654b3bd6525d62848c27c6cd1b584b9825601
joszamama <joszamama@gmail.com>
2023-02-23 11:24:50 +0100

    feat: comparison between exact and aprox methods

--- src/main/java/com/dominatingset/Dominating.java
+++ src/main/java/com/dominatingset/Dominating.java
@@ -1,23 +1,73 @@
 package com.dominatingset;
 
 import org.jgrapht.Graph;
-import org.jgrapht.alg.interfaces.VertexCoverAlgorithm.VertexCover;
+import org.jgrapht.alg.vertexcover.GreedyVCImpl;
 import org.jgrapht.alg.vertexcover.RecursiveExactVCImpl;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.SimpleGraph;
 
 public class Dominating {
 
-    public static void dominatingSetOfGraph(Graph<String, DefaultEdge> graph) {
-        // Compute the minimum dominating set of the graph
-        RecursiveExactVCImpl<String, DefaultEdge> algorithm = new RecursiveExactVCImpl<>(graph);
-        VertexCover<String> vertexCover = algorithm.getVertexCover();
+    // The RecursiveExactVCImpl algorithm computes the exact minimum vertex cover of
+    // a graph using a recursive branching strategy. The algorithm has an
+    // exponential time complexity in the worst case, with a running time of O(2^n),
+    // where n is the number of vertices in the graph. However, in practice, the
+    // algorithm performs much better on real-world graphs, often finding the exact
+    // minimum vertex cover in a reasonable amount of time.
 
-        // Print the minimum dominating set
-        System.out.println("Minimum dominating set:");
-        for (String vertex : vertexCover) {
+    // The space complexity of the algorithm is also exponential in the worst case,
+    // with a space usage of O(2^n). This is because the algorithm constructs a
+    // binary tree of depth n, where each node represents a possible solution to the
+    // vertex cover problem. The space used by the algorithm is proportional to the
+    // number of nodes in this tree, which is exponential in the worst case.
+
+    public static void exactDominatingSet(Graph<String, DefaultEdge> graph) {
+        RecursiveExactVCImpl<String, DefaultEdge> exactAlgorithm = new RecursiveExactVCImpl<>(graph);
+        long exactStartTime = System.nanoTime();
+        exactAlgorithm.getVertexCover();
+        long exactEndTime = System.nanoTime();
+        long exactTimeElapsed = exactEndTime - exactStartTime;
+        long exactMemoryUsed = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+
+        // Print the results
+        System.out.println("Exact vertex cover:");
+        for (String vertex : exactAlgorithm.getVertexCover()) {
             System.out.println(vertex);
         }
+        System.out.println("Exact time elapsed: " + exactTimeElapsed + " nanoseconds");
+        System.out.println("Exact memory used: " + exactMemoryUsed + " bytes");
+    }
+
+    // The time complexity of the GreedyVCImpl algorithm is O(|E|), where |E| is the
+    // number of edges in the graph. This is because the algorithm iterates over all
+    // the edges in the graph and selects the endpoints of each edge that are not
+    // yet covered by the vertex cover.
+
+    // The space complexity of the GreedyVCImpl algorithm is O(|V|), where |V| is
+    // the number of vertices in the graph. This is because the algorithm maintains
+    // a set of candidate vertices that have not yet been added to the vertex cover,
+    // which could potentially include all the vertices in the graph.
+
+    public static void aproxDominatingSet(Graph<String, DefaultEdge> graph) {
+        // Compute the vertex cover of the graph using the greedy algorithm
+        GreedyVCImpl<String, DefaultEdge> approximateAlgorithm = new GreedyVCImpl<>(graph);
+        long approximateStartTime = System.nanoTime();
+        approximateAlgorithm.getVertexCover();
+        long approximateEndTime = System.nanoTime();
+        long approximateTimeElapsed = approximateEndTime - approximateStartTime;
+        long approximateMemoryUsed = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+
+        // Print the results
+        System.out.println("Approximate vertex cover:");
+        for (String vertex : approximateAlgorithm.getVertexCover()) {
+            System.out.println(vertex);
+        }
+        System.out.println("Approximate time elapsed: " + approximateTimeElapsed + " nanoseconds");
+        System.out.println("Approximate memory used: " + approximateMemoryUsed + " bytes");
+    }
+
+    public static void compareExactAndAprox(Graph<String, DefaultEdge> graph) {
+
     }
 
     public static void main(String[] args) {
@@ -40,7 +90,20 @@ public class Dominating {
         graph.addEdge("D", "E");
         graph.addEdge("E", "F");
 
-        // Compute the minimum dominating set of the graph
-        dominatingSetOfGraph(graph);
+        // Compute the minimum dominating set of the graph using exact algorithm
+        exactDominatingSet(graph);
+
+        System.out.println("--------------------");
+
+        // Compute the minimum dominating set of the graph using approximate algorithm
+        aproxDominatingSet(graph);
+
+        // Overall, the GreedyVCImpl algorithm is relatively efficient in both time and
+        // space complexity, especially compared to the RecursiveExactVCImpl algorithm
+        // which has an exponential worst-case time complexity. However, it should be
+        // noted that the GreedyVCImpl algorithm may not always compute the exact
+        // minimum vertex cover of the graph, since it uses a heuristic approach to
+        // select the vertices for the vertex cover.
+        
     }
 }
