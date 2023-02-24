d9af53701b00a9303f67653d754817a234032da5
joszamama <joszamama@gmail.com>
2023-02-24 17:13:19 +0100

    refactor: spotter error in impl

--- src/main/java/com/dominatingset/Dominating.java
+++ src/main/java/com/dominatingset/Dominating.java
@@ -1,42 +1,28 @@
 package com.dominatingset;
 
+import java.util.Set;
+
 import org.jgrapht.Graph;
 import org.jgrapht.graph.DefaultEdge;
-import org.jgrapht.graph.SimpleGraph;
 
 public class Dominating {
 
     public static void main(String[] args) {
         // Create an undirected graph
-        Graph<Integer, DefaultEdge> graph = new SimpleGraph<>(DefaultEdge.class);
+        Graph<Integer, DefaultEdge> graph = GraphGenerator.generateGraph(4, 1);
 
-        // Add vertices to the graph
-        graph.addVertex(1);
-        graph.addVertex(2);
-        graph.addVertex(3);
-        graph.addVertex(4);
-        graph.addVertex(5);
-        graph.addVertex(6);
-        graph.addVertex(7);
-        graph.addVertex(8);
-
-        // Add edges to the graph
-        graph.addEdge(1, 2);
-        graph.addEdge(1, 3);
-        graph.addEdge(1, 4);
-        graph.addEdge(2, 3);
-        graph.addEdge(2, 4);
-        graph.addEdge(2, 5);
-        graph.addEdge(3, 4);
-        graph.addEdge(3, 5);
-        graph.addEdge(5, 6);
-        graph.addEdge(5, 7);
-        graph.addEdge(5, 8);
-        graph.addEdge(6, 7);
+        // Plot the graph
+        GraphPlotter.plotUndirectedGraph(graph);
 
         System.out.println("--------------------");
         System.out.println("Compute the Minimum Dominating Set of a Graph");
         System.out.println("--------------------");
+
+        // Find the dominating set
+        Set<Integer> dominatingSet = null;
+
+        // Print the dominating set
+        System.out.println("The dominating set is: " + dominatingSet);
 
     }
 }
