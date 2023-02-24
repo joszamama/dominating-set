e20d585ce629eddd3f8618a9e2234c8a401a799e
joszamama <joszamama@gmail.com>
2023-02-24 16:58:09 +0100

    feat: add graph plotter and fix generator

--- src/main/java/com/dominatingset/GraphGenerator.java
+++ src/main/java/com/dominatingset/GraphGenerator.java
@@ -1,88 +1,37 @@
 package com.dominatingset;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.function.Supplier;
-
 import org.jgrapht.Graph;
-import org.jgrapht.generate.GnpRandomGraphGenerator;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.SimpleGraph;
 
 public class GraphGenerator {
 
-    public static Graph<String, DefaultEdge> generateGraph(int n, double p) {
-        Supplier<String> vertexSupplier = new Supplier<String>() {
-            private int id = 0;
+    public static Graph<Integer, DefaultEdge> generateGraph(int nodes, double probability) {
+        // Create an undirected graph
+        Graph<Integer, DefaultEdge> graph = new SimpleGraph<>(DefaultEdge.class);
 
-            @Override
-            public String get() {
-                return "v" + id++;
-            }
-        };
-        Supplier<DefaultEdge> edgeSupplier = new Supplier<DefaultEdge>() {
-            @Override
-            public DefaultEdge get() {
-                return new DefaultEdge();
-            }
-        };
-        GnpRandomGraphGenerator<String, DefaultEdge> generator = new GnpRandomGraphGenerator<>(n, p);
-        Graph<String, DefaultEdge> graph = new SimpleGraph<>(vertexSupplier, edgeSupplier, false);
-        generator.generateGraph(graph);
-        return graph;
-    }
-
-    public static void graphToFile(Graph<String, DefaultEdge> graph, String filename) {
-        try {
-            FileWriter writer = new FileWriter("./src/main/java/com/dominatingset/graphs/" + filename);
-            // Write the vertices
-            for (String vertex : graph.vertexSet()) {
-                writer.write(vertex + "\n");
-            }
-            // Write the edges
-            writer.write("--------------" + "\n");
-            for (DefaultEdge edge : graph.edgeSet()) {
-                String source = graph.getEdgeSource(edge);
-                String target = graph.getEdgeTarget(edge);
-                writer.write("{" + source + " : " + target + "}" + "\n");
-            }
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+        // Add vertices to the graph
+        for (int i = 1; i <= nodes; i++) {
+            graph.addVertex(i);
         }
-    }
 
-    public static Graph<String, DefaultEdge> fileToGraph(String filename) {
-        Graph<String, DefaultEdge> graph = new SimpleGraph<>(DefaultEdge.class);
-        try {
-            BufferedReader reader = new BufferedReader(
-                    new FileReader("./src/main/java/com/dominatingset/graphs/" + filename));
-            String line = reader.readLine();
-            while (line != null && !line.equals("--------------")) {
-                graph.addVertex(line);
-                line = reader.readLine();
+        // Add edges to the graph
+        for (int i = 1; i <= nodes; i++) {
+            for (int j = i + 1; j <= nodes; j++) {
+                if (Math.random() < probability) {
+                    graph.addEdge(i, j);
+                }
             }
-            line = reader.readLine(); // skip "--------------" line
-            while (line != null) {
-                String[] parts = line.replaceAll("[{} ]", "").split(":");
-                String source = parts[0];
-                String target = parts[1];
-                graph.addEdge(source, target);
-                line = reader.readLine();
-            }
-            reader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
+
         return graph;
     }
 
     public static void main(String[] args) {
-        Graph<String, DefaultEdge> graph = generateGraph(10, 0.5);
-        graphToFile(graph, "test.txt");
-        Graph<String, DefaultEdge> graph2 = fileToGraph("test.txt");
-        System.out.println(graph2);
+        // Generate a graph
+        Graph<Integer, DefaultEdge> graph = generateGraph(50, 0.05);
+
+        // Plot the graph
+        GraphPlotter.plotUndirectedGraph(graph);
     }
 }
