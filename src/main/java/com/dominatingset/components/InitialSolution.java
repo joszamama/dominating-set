f045a5210d6296b7c32323faacb6ae154ac1a388
joszamama <joszamama@gmail.com>
2023-03-03 21:47:24 +0100

    feat: MDSP instance for multiparam functions

---
+++ src/main/java/com/dominatingset/components/InitialSolution.java
@@ -0,0 +1,14 @@
+package com.dominatingset.components;
+
+import java.util.Set;
+
+public class InitialSolution {
+
+    public static Set<Integer> InitialSolution1(boolean[][] graph) {
+        return null;
+    }
+
+    public static Set<Integer> InitialSolution2(boolean[][] graph) {
+        return null;
+    }
+}
